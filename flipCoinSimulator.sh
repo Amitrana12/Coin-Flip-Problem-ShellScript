3ded56720a1f37d03160c5bd05bb6eb90f49fa2d
amit <[amirana14325@gmail.com]>
2021-02-12 01:36:00 +0530

    Adding:: number of wins head and tails  in 20 time flip coin

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -1,10 +1,20 @@
 #!/bin/bash -x
-flip=$((RANDOM%2))
-if (( $flip==1 ))
-then
-	echo "head"
-else
-	echo "tails"
-fi
+coutHeadWin=0
+countTailsWin=0
+for (( i=1; i<=20; i++ ))
+do
+	flip=$((RANDOM%2))
+
+	if (( $flip==1 ))
+	then
+		eecho "head win"
+		((countHeadWin++))
+	else
+		echo "tails"
+		((countTailsWin++))
+	fi
+done
+echo "head win count out of 20 is=" $countHeadWin
+echo "tails win count out of 20 is=" $countTailsWin
 
 
